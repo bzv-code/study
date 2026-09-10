a29aa5386705df7c013f085125dc0c38b8338456
bzv-code <brezgunov.kv@gmail.com>
2026-09-11 00:29:47 +0300

    Delete ba20260830 directory

--- ba20260830/database/__init__.py
+++
@@ -1,5 +0,0 @@
-
-from .client_clickhouse import ClickHouseClient, client
-from .config_clickhouse import ClickHouseConfig, config
-
-__all__ = ['ClickHouseClient', 'client', 'ClickHouseConfig', 'config']
